363954471bf05dba1f04ee6b274d093aa54e5c34
065119017-BagusWahyuM <bagusmediansyah11@gmail.com>
2022-06-13 18:38:54 +0700

    firstcommit

---
+++ flutter_application_2/lib/data/rental.dart
@@ -0,0 +1,55 @@
+// ignore: camel_case_types
+class rental {
+  String name, sewa, spesifikasi, image;
+
+  rental(
+      {required this.name,
+      required this.sewa,
+      required this.spesifikasi,
+      required this.image});
+}
+
+List<rental> dataRental = [
+  rental(
+      name: 'Avanza Tipe G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/avanza.jpg'),
+  rental(
+      name: 'Foutuner 2.4 G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/fortuner.jpg'),
+  rental(
+      name: 'Foutuner 2.4 G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/fortuner.jpg'),
+  rental(
+      name: 'Foutuner 2.4 G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/fortuner.jpg'),
+  rental(
+      name: 'Foutuner 2.4 G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/fortuner.jpg'),
+  rental(
+      name: 'Foutuner 2.4 G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/fortuner.jpg'),
+  rental(
+      name: 'Foutuner 2.4 G MT',
+      sewa: 'RP1jt/Bulan',
+      spesifikasi:
+          'Kapasitas Mesin : 1300 CC, Tenaga Maksimum : 96 PS @6.000 rpm, Torsi Maksimum : 11.9 kgm @4400 rpm, Tranmisi : 4-Speed Automatic',
+      image: 'assets/fortuner.jpg'),
+];
